ef1431af886664c0f79e901b582aba7c34d1d5fc
blorguscake <122128098+blorguscake@users.noreply.github.com>
2023-11-03 14:22:57 -0500

    autonomoose outline
    this should be a general outline of goals for having the husky lens (which has its own program and will input values of ID and distance on its own into this code) input its data into our code. I also want to set this up with being able to detect which of 3 positions the object is in and associating it with a memorized apriltag (this apriltag isn't setup but should be the easier latter part) message me for any questions and further goal discussion.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/auto.java
@@ -11,10 +11,74 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
-
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.UartDevice;
+import com.qualcomm.robotcore.hardware.UartDeviceConfig;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class auto {
+
+
+    @Autonomous(name="HuskyLens AprilTag Reader", group="Sample")public class HuskyLensAprilTagReader extends LinearOpMode {
+
+        private UartDevice huskyLensUart;
+        private final ElapsedTime runtime = new ElapsedTime();
+
+        @Override
+        public void runOpMode() {
+            telemetry.addData("Status", "Initialized");
+            telemetry.update();
+
+            // Initialize UART device
+            huskyLensUart = hardwareMap.get(UartDevice.class, "huskyLensUart");
+            UartDeviceConfig uartConfig = new UartDeviceConfig();
+            uartConfig.baudRate = UartDeviceConfig.BaudRate.BAUD_RATE_9600;
+            huskyLensUart.setConfig(uartConfig);
+
+            waitForStart();
+            runtime.reset();
+
+            while (opModeIsActive()) {
+                // Read data from HuskyLens
+                byte[] buffer = new byte[20];  // Adjust size based on expected data
+                int read = huskyLensUart.read(buffer, buffer.length);
+
+                if (read > 0) {
+                    // Assuming the buffer now contains some data regarding the detected AprilTags
+                    // Extract ID or relevant data
+                    int aprilTagID = extractAprilTagID(buffer);
+                    telemetry.addData("AprilTag ID", aprilTagID);
+
+                    // For distance calculation, you'd need a method to process the tag's size or position
+                    // double distance = calculateDistance(buffer);
+                    // telemetry.addData("Distance", distance);
+                }
+
+                telemetry.update();
+            }
+        }
+
+        // This method is a placeholder. Actual extraction depends on HuskyLens's data structure
+        private int extractAprilTagID(byte[] data) {
+            // Implement the extraction logic based on the HuskyLens's protocol for AprilTags
+            return 0;  // Placeholder
+        }
+
+        public double calculateDistance(byte[] data) {
+            // Implement distance calculation based on tag's size or position in the image
+            // You'll need to know the data structure provided by HuskyLens for this calculation
+            // For example, if the size of the tag is in the second byte of the data:
+            if (data.length > 1) {
+                double tagSize = data[1];  // Assuming tag size is in the second byte
+                // Implement your distance calculation logic using tagSize
+                // Example: distance = someFormula(tagSize);
+                return distance;
+            }
+            return 0.0;  // Placeholder
+    }
 }
 
 /*
@@ -39,7 +103,5 @@ public class auto {
  *  There are other ways to perform encoder based moves, but this method is probably the simplest.
  *  This code uses the RUN_TO_POSITION mode to enable the Motor controllers to generate the run profile
  *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
