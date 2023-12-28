2177d3194e1ae1788a4f9573b35d6898f5e0fc91
AriDela <55457916+Nitr0gue@users.noreply.github.com>
2023-12-27 21:12:11 -0500

    constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -46,7 +46,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.889765; // in
     public static double GEAR_RATIO = .2; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 11.75; // in
+    public static double TRACK_WIDTH = 11.5; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -67,8 +67,8 @@ public class DriveConstants {
      */
     public static double MAX_VEL = 30;
     public static double MAX_ACCEL = 30;
-    public static double MAX_ANG_VEL = Math.toRadians(60);
-    public static double MAX_ANG_ACCEL = Math.toRadians(60);
+    public static double MAX_ANG_VEL = Math.toRadians(0.7);
+    public static double MAX_ANG_ACCEL = Math.toRadians(1.05);
 
     /*
      * Adjust the orientations here to match your robot. See the FTC SDK documentation for details.
