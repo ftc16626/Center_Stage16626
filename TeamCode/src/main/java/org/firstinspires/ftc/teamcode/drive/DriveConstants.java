27533fc0651d243c3cdf53dd18d4c06991d73d57
blorguscake <122128098+blorguscake@users.noreply.github.com>
2023-12-27 19:06:01 -0600

    updates to drive constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -45,7 +45,7 @@ public class DriveConstants {
      * convenience. Make sure to exclude any gear ratio included in MOTOR_CONFIG from GEAR_RATIO.
      */
     public static double WHEEL_RADIUS = 1.889765; // in
-    public static double GEAR_RATIO = .052; // output (wheel) speed / input (motor) speed
+    public static double GEAR_RATIO = .2; // output (wheel) speed / input (motor) speed
     public static double TRACK_WIDTH = 11.75; // in
 
     /*
@@ -54,9 +54,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
-    public static double kA = 0.2;
-    public static double kStatic = 0;
+    public static double kV = .033055 / rpmToVelocity(MAX_RPM);
+    public static double kA = 0.32;
+    public static double kStatic = .07;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
