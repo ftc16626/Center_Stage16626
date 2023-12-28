28e65e5232e882b96e9bdee750113ad452b0072b
blorguscake <122128098+blorguscake@users.noreply.github.com>
2023-12-27 20:54:38 -0600

    constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -46,7 +46,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.889765; // in
     public static double TRACK_WIDTH = 11.5; // in
-    public static double GEAR_RATIO = 7; // output (wheel) speed / input (motor) speed used to be .2
+    public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed used to be .2
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -54,9 +54,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = .033055 / rpmToVelocity(MAX_RPM);
-    public static double kA = 0.32;
-    public static double kStatic = .07;
+    public static double kV = 1 / rpmToVelocity(MAX_RPM);
+    public static double kA = 0;
+    public static double kStatic = 0;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
@@ -67,8 +67,8 @@ public class DriveConstants {
      */
     public static double MAX_VEL = 30;
     public static double MAX_ACCEL = 30;
-    public static double MAX_ANG_VEL = Math.toRadians(0.7);
-    public static double MAX_ANG_ACCEL = Math.toRadians(1.05);
+    public static double MAX_ANG_VEL = Math.toRadians(184);
+    public static double MAX_ANG_ACCEL = Math.toRadians(184);
 
     /*
      * Adjust the orientations here to match your robot. See the FTC SDK documentation for details.
