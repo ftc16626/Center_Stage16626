3557352c8e10407f0ff37764d967dbf4bb554494
blorguscake <122128098+blorguscake@users.noreply.github.com>
2024-01-05 16:48:58 -0600

    read last

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -66,7 +66,7 @@ public class DriveConstants {
      * inches.
      */
     public static double MAX_VEL = 15;
-    public static double MAX_ACCEL = 15;
+    public static double MAX_ACCEL = 10;
     public static double MAX_ANG_VEL = Math.toRadians(184);
     public static double MAX_ANG_ACCEL = Math.toRadians(184);
 
